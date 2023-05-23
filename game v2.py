e7838d5c28a76b13788acf6e5a138fd483599804
Vinaya Wani <93176721+vwani@users.noreply.github.com>
2023-05-23 21:25:56 +0530

    Initial commit

---
+++ game v2.py
@@ -0,0 +1,224 @@
+# import modules
+import pygame
+import sys
+import random
+import math
+
+# initialise pygame
+pygame.init()
+
+# screen dimensions
+res = (800, 800)
+
+# create screen
+screen = pygame.display.set_mode(res)
+
+# title
+pygame.display.set_caption('FOODIE !!')
+
+# icon
+icon = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\icon.png')
+pygame.display.set_icon(icon)
+
+# background image
+backdrop = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\backdrop.png')
+
+# font
+font = pygame.font.Font('freesansbold.ttf', 36)
+
+# player
+PlayerImg = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\MalePlayer.png')
+
+PlayerX = 336
+PlayerY = 662
+PlayerChangeX = 0
+PlayerChangeY = 0
+
+
+def player(x, y):
+    screen. blit(PlayerImg, (x, y))
+
+
+# healthy food
+HealthyFoodNum = 3
+
+HealthyFood1 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\HealthyFood1.png')
+HealthyFood2 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\HealthyFood2.png')
+HealthyFood3 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\HealthyFood3.png')
+HealthyFood4 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\HealthyFood4.png')
+HealthyFoodArr = [HealthyFood1, HealthyFood2, HealthyFood3, HealthyFood4]
+
+# unhealthy food
+UnHealthyFoodNum = 1
+
+UnHealthyFood1 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\UnHealthyFood1.png')
+UnHealthyFood2 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\UnHealthyFood2.png')
+UnHealthyFood3 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\UnHealthyFood3.png')
+UnHealthyFood4 = pygame.image.load('F:\\Vinaya\\Python Projects\\game\\Media\\UnHealthyFood4.png')
+UnHealthyFoodArr = [UnHealthyFood1, UnHealthyFood2, UnHealthyFood3, UnHealthyFood4]
+
+# food
+FoodArr = []
+
+FoodX = []
+FoodY = []
+
+HNum = HealthyFoodNum
+UHNum = UnHealthyFoodNum
+while HNum:
+    num = random.randint(0, 3)
+    while HealthyFoodArr[num] in FoodArr:
+        num = random.randint(0, 3)
+    FoodArr.append(HealthyFoodArr[num])
+    HNum -= 1
+while UHNum:
+    num = random.randint(0, 3)
+    while UnHealthyFoodArr[num] in FoodArr:
+        num = random.randint(0, 3)
+    FoodArr.append(UnHealthyFoodArr[num])
+    UHNum -= 1
+
+for i in range(len(FoodArr)):
+
+    num = random.randrange(10, 736, 100)
+    while num in FoodX:
+            num = random.randrange(10, 736, 80)
+    FoodX.append(num)
+    FoodY.append(random.randint(10, 100))
+
+FoodChangeX = 0
+FoodChangeY = 0.5
+
+
+def food(img, x, y):
+    screen.blit(img, (x, y))
+
+
+# collision
+
+
+def IsCollision(px, py, fx, fy):
+    distX = px + 20 <= fx <= px + 108
+    distY = py + 20 <= fy <= py + 108
+    return distX and distY
+
+
+# health
+health = 10
+maxhealth = 10
+
+
+def DisplayHealth(x):
+    screen.blit(font.render('Health: '+str(x), True, (255, 255, 255)), (0, 0))
+
+
+# game loop
+running = True
+while running:
+
+    # event check
+    for event in pygame.event.get():
+
+        # exit game loop 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # player movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                PlayerChangeX = 0.2
+
+            elif event.key == pygame.K_LEFT:
+                PlayerChangeX = -0.2
+
+    # food movement
+    for i in range(len(FoodArr)):
+        FoodY[i] += FoodChangeY
+
+        if FoodY[i] >= 736:
+            FoodY[i] = 0
+            num = random.randrange(10, 736, 100)
+            while num in FoodX:
+                    num = random.randrange(10, 736, 100)
+            FoodX[i] = num
+
+            if FoodArr[i] in HealthyFoodArr:
+                num = random.randint(0, 3)
+                while HealthyFoodArr[num] in FoodArr:
+                        num = random.randint(0, 3)
+                FoodArr[i] = HealthyFoodArr[num]
+
+            elif FoodArr[i] in UnHealthyFoodArr:
+                num = random.randint(0, 3)
+                while UnHealthyFoodArr[num] in FoodArr:
+                        num = random.randint(0, 3)
+                FoodArr[i] = UnHealthyFoodArr[num]
+
+        # collision
+        collision = IsCollision(PlayerX, PlayerY, FoodX[i], FoodY[i])
+        if collision:
+            FoodY[i] = 0
+
+            num = random.randrange(10, 736, 100)
+            while num in FoodX:
+                num = random.randrange(10, 736, 100)
+            FoodX[i] = num
+
+            if FoodArr[i] in HealthyFoodArr:
+                num = random.randint(0, 3)
+                while HealthyFoodArr[num] in FoodArr:
+                        num = random.randint(0, 3)
+                FoodArr[i] = HealthyFoodArr[num]
+
+                health += 1
+
+            elif FoodArr[i] in UnHealthyFoodArr:
+                num = random.randint(0, 3)
+                while UnHealthyFoodArr[num] in FoodArr:
+                        num = random.randint(0, 3)
+                FoodArr[i] = UnHealthyFoodArr[num]
+
+                health -= 2
+
+    # player movement
+    PlayerX += PlayerChangeX
+
+    if PlayerX >= 800 - 128:
+        PlayerX = 800 - 128
+
+    elif PlayerX <= 0:
+        PlayerX = 0
+
+    # display health
+    DisplayHealth(health)
+
+    # game over
+    if health <= 0:
+        running = False
+        FoodY = [-500 for x in range(len(FoodArr))]
+
+    # motion
+    player(PlayerX, PlayerY)
+
+    for i in range(len(HealthyFoodArr)):
+        food(FoodArr[i], FoodX[i], FoodY[i])
+
+    # background image
+    screen.blit(backdrop, (0, 0))
+
+    # update screen
+    pygame.display.update()
+
+while not running:
+
+    # event check
+    for event in pygame.event.get():
+
+        # exit game loop
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # update screen
+    pygame.display.update()
